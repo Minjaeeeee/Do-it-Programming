7336e3cc8f6fd6cc844a1410b1fcf5a6ea253685
Minjaeeeee <kmj6832596@naver.com>
2021-11-12 11:25:00 +0900

    Add Java File [variable.java]

---
+++ variable.java
@@ -0,0 +1,34 @@
+// 
+// public class variable
+// {
+//     public static void main(String[ ] args)
+//     {
+//         int age;
+//         age=22;
+//         System.out.println(age);
+//     }
+// }
+// */
+
+// /*
+// public class variable //변수를 초기화하고 출력해보기
+// {
+//     public static void main(String[ ] args)
+//     {
+//         int level=10;
+//         System.out.println(level);
+//     }
+// }
+// */
+
+
+public class variable
+{
+    public static void main(String[ ] args)
+    {
+       byte bVal = 20;
+       short Sval= 10;
+       System.out.println(bVal+Sval);
+    }
+}
+
