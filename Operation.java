f4865322365d49c27c3df5d12f2ba0ca3be90235
Minjaeeeee <kmj6832596@naver.com>
2021-11-12 12:12:06 +0900

    Add Java File [Operation.java]

---
+++ Operation.java
@@ -0,0 +1,78 @@
+
+// public class Operation
+// {
+//     public static void main(String[] args)
+//     {
+//         int mathscore = 90;
+//         int engscore = 70;
+
+//         int totalscore = mathscore + engscore;
+//         System.out.println(totalscore)
+
+//         double avgscore = totalscore / 2.0;
+//         System.out.println(avgscore);
+//     }
+// }
+
+
+
+// public class Operation
+// {
+//     public static void main(String[] args)
+//     {
+//         int num=10;
+
+//         System.out.println(+num);
+//         System.out.println(-num);
+        
+//         num= -num; //이걸 해야 실제 값이 바뀐다.
+//         System.out.println(num);
+//     }
+// }
+
+
+// public class Operation
+// {
+//     public static void main(String[] args)
+//     {
+//         int mathscore=90;
+//         int engscore=70;
+
+//         int totalscore=mathscore+engscore;
+//         System.out.println(totalscore);
+
+//         double avgscore = totalscore / 2.0;
+//         System.out.println(avgscore);
+//     }
+// }
+
+// public class Operation
+// {
+//     public static void main(String[ ] args)
+//     {
+//     int  num1=10;
+//     int i=2;
+
+//     boolean value = ((num1=num1+10)<10) && ((i=i+2)<10);
+//     System.out.println(value);
+//     System.out.println(num1);
+//     System.out.println(i);
+
+//     value = ((num1 = num1+10)>10) || ((i=i+2)<10);
+
+//      }
+// }
+
+
+
+public class Operation
+{
+    public static void main(String[] args)
+    {
+        int fatherage=50;
+        int motherage=47;
+
+        char ch;
+        ch = (fatherage>motherage) ? 't' : 'f';
+        System.out.println(ch);
+    }
