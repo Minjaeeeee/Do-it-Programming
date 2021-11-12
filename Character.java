b5578856e0ae757a5efde1e1719e55f85b9cfc68
Minjaeeeee <kmj6832596@naver.com>
2021-11-12 11:36:09 +0900

    Add Java File [Character.java]

---
+++ Character.java
@@ -0,0 +1,65 @@
+/*
+public class Character  // 1분 복습 문제
+{
+    public static void main(String[ ] args)
+    {
+        char ch1 = 'Z';
+        System.out.println(ch1);
+        System.out.println((int)ch1);
+
+        char ch2 = 38; //문자 66은 아스키 코드로 변환
+        System.out.println(ch2);
+       
+        int ch3 = 97;
+        System.out.println(ch3); //정수형 int 이므로 67 출력
+        System.out.println((char)ch3); //문자열 67은 C이므로 C 출력 
+
+    }
+}
+*/
+
+/*
+public class Character   //문자형 연습
+{
+    public static void main(String[ ] args)
+    {
+        char ch1='한';
+        char ch2='\uD55C';
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+    }
+}
+*/
+/*
+public class Character   //문자형 연습
+{
+    public static void main(String[ ] args)
+    {
+        int a = 65;
+        int b = -66;
+
+        char a2 = 65;
+
+        System.out.println((char)a);
+        System.out.println((char)b);
+        System.out.println(a2);
+
+    }
+}
+*/
+
+
+public class Character  //문자형 연습
+{
+    public static void main(String[ ] args)
+    {
+       double dnum = 3.14;
+       float fnum = 3.14F; //F는 식별자로 사용된다.
+
+       System.out.println(dnum);
+       System.out.println(fnum);
+
+    }
+}
