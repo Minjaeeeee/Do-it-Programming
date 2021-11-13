124a17523074c3011a037d59e2dda674404b85ae
Minjaeeeee <kmj6832596@naver.com>
2021-11-13 20:51:12 +0900

    Add Java File [Student.java]

---
+++ Student.java
@@ -0,0 +1,29 @@
+// Student 클래스에 main() 함수 추가하기!
+
+// public class Student  //Student 클래스 
+// {
+//     int studentID;
+//     String studentName="김민재";
+//     int grade;
+//     String address;
+
+    
+//     public String getStudentName( ) //학생 이름을 반환하는 메서드이다!
+//     {
+//         return studentName; //반환하여도 String 형으로 반환이 된다.
+//     }
+    
+
+
+
+//     public static void main(String[ ] args) //여기가 main함수이다!
+//     {
+//         Student studentAhn = new Student(); //studentAhn이라는 새로운 Student클래스를 만드는 것!
+//         studentAhn.studentName = "안연수"; //스튜던트 네임을 반환한다.
+
+//         System.out.println(studentAhn.studentName);
+//         System.out.println(studentAhn.getStudentName());
+//     }
+// }
+
+
