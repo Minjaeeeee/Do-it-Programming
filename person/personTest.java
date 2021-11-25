b3c19785f740bdf0948532d3ba846434d253c1a6
Minjaeeeee <94120988+Minjaeeeee@users.noreply.github.com>
2021-11-25 19:05:29 +0900

    Add Java File [personTest.java] updated

--- person/personTest.java
+++ person/personTest.java
@@ -4,7 +4,11 @@ public class personTest //생성자를 생성
 {
     public static void main(String[] args)
     {
-        person personLee = new person(); //생성자를 만든다!
+        person personKim = new person(); //디폴트 생성자!
+        personKim.name = "김유신";
+        personKim.weight = 85.5F; //인스턴스 변수값을 따로 초기화를 해서 생성하는 타입!
+        personKim.height = 180.0F;
+
+        person personLee = new person("이순신",175,75); //인스턴스 변수 초기화와 동시에 클래스 생성
     }    
 }
- 
