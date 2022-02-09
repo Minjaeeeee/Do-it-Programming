e0c1b488fe413ebb8cee407b36ddc1ebb28a0989
Minjaeeeee <94120988+Minjaeeeee@users.noreply.github.com>
2022-02-09 17:33:08 +0900

    Add files via upload

---
+++ arraylist/StudentTest.java
@@ -0,0 +1,20 @@
+package arraylist;
+
+public class StudentTest 
+{
+    public static void main(String[] args)
+    {
+        Student studentLee = new Student(1001,"LEE"); //학번과 학생이름을 적어 놓습니다.
+        studentLee.addSubject("국어", 100);
+        studentLee.addSubject("수학", 50);
+
+        Student studentKim = new Student(1002,"KIM");
+        studentKim.addSubject("국어",70);
+        studentKim.addSubject("수학", 85);
+        studentKim.addSubject("영어", 100);
+
+        studentLee.showStudentInfo();
+        System.out.println("======================");
+        studentKim.showStudentInfo();
+    }
+}
---
+++ arraylist/Subject.java
@@ -0,0 +1,28 @@
+package arraylist;
+
+
+public class Subject  //학생들의 총 점수 합계의 클래스를 생성합니다.
+{
+    private String name;
+    private int scorePoint;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getScorePoint()
+    {
+        return scorePoint;
+    }
+    
+    public void setScorePoint(int scorePoint)
+    {
+        this.scorePoint = scorePoint;
+    }
+}
