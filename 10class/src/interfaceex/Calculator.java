78977521d87d73f88cf06056e4c930c4a414bc45
Min Jae Kim <kmj6832596@naver.com>
2022-02-22 16:15:41 +0900

    Add files via upload

---
+++ 10class/src/interfaceex/Calculator.java
@@ -0,0 +1,20 @@
+package interfaceex;
+
+public abstract class Calculator implements Calc  //추상클래스입니다.
+{
+
+	@Override
+	public int add(int num1, int num2) 
+	{
+		return num1 + num2;
+		
+	}
+
+	@Override
+	public int substract(int num1, int num2) 
+	{
+		return num1 - num2;
+	}
+
+
+}
