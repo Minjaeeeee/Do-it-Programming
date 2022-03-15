ebe7f347494deefdb48241b44a8e299a166b5fe8
Min Jae Kim <kmj6832596@naver.com>
2022-03-15 11:20:41 +0900

    Update CoffeeWho.java

--- kimcoffee/CoffeeWho.java
+++ kimcoffee/CoffeeWho.java
@@ -4,8 +4,8 @@ public class CoffeeWho
 {
     public static void main(String[] args)
     {
-        Person firstCustomer = new Person("김민재",24000);
-        Starbucks starbucks = new Starbucks();
+        Person firstCustomer = new Person("김민재",24000);  //손님 한명이 24000원을 가지고있는 것을 입력
+        Starbucks starbucks = new Starbucks(); //스타벅스 인스턴스를 하나개발
         Backdabang backdabang = new Backdabang();
 
         firstCustomer.VisitStarbucks(starbucks, 25000, "커피");
