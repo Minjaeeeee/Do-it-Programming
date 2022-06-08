db6b2dac4e96b752df08e4b624c9577f9d26af78
Min Jae Kim <kmj6832596@naver.com>
2022-06-09 01:18:00 +0900

    Create VipCustomer(부모).java

---
+++ inheritance/VipCustomer(부모).java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class VIPCustomer extends Customer
+{
+  private int agentID;
+  double saleRatio;
+  
+  public VIPCustomer()
+  {
+    customerGrade = "VIP";
+    bonusRatio = 0.05; //상위클래스에서 private 변수이므로 오류가 발생한다! 
+    //메서드 오버라이딩으로 해결이 가능하다!
+    saleRatio = 0.1;
+  }
+  
+  public int getAgendID()
+  {
+    return agentID;
+  }
