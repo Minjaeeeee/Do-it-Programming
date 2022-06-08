a780f0bca274a44c17b86cb1880efa435136bc9d
Min Jae Kim <kmj6832596@naver.com>
2022-06-08 23:25:07 +0900

    Update personTest.java

--- person/personTest.java
+++ person/personTest.java
@@ -4,7 +4,7 @@ public class personTest //생성자를 생성
 {
     public static void main(String[] args)
     {
-        person personKim = new person(); //디폴트 생성자!
+        Person personKim = new person(); //디폴트 생성자!
         personKim.name = "김유신";
         personKim.weight = 85.5F; //인스턴스 변수값을 따로 초기화를 해서 생성하는 타입!
         personKim.height = 180.0F;
