765b1ed261fe06270747e7afd71302f233a9484a
Min Jae Kim <kmj6832596@naver.com>
2022-06-09 01:35:55 +0900

    Update Customer.java

--- inheritance/Customer.java
+++ inheritance/Customer.java
@@ -8,25 +8,8 @@ public class Customer
     int bonusPoint; //보너스 포인트
     double bonusRatio; //적립 비율
 
-    public Customer(int customerID, String customerName) //생성자(2개의 인자를 받고있다)
-    {
-        this.customerID = customerID;
-        this.customerName = customerName;
-        customerGrade="SILVER"; //기본 등급이 실버이다(인스턴스가 생성될때 기본 등급 실버)
-        bonusRatio = 0.01; //기본 보너스 포인트 기본 적립비율
-    }
-
-    public int calcPrice(int price) //보너스 포인트 계산
-    {
-        bonusPoint += price * bonusRatio;
-        return price;
-    }
-
-    public String showCustomerInfo()
-    {
-        return customerName + " 님의 등급은" + customerGrade + "이며, 보너스포인트는" + bonusPoint + "입니다.";
-    }
-
+    ----------(생성자, showInfo, 등등 기존 코드들은 생략)------------
+   
     public int getCustomerID()
     {
         return customerID;
