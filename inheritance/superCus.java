305358d402704ade9055a87e22c2426d97468d48
Min Jae Kim <kmj6832596@naver.com>
2022-06-09 01:52:55 +0900

    Create superCus.java

---
+++ inheritance/superCus.java
@@ -0,0 +1,9 @@
+----위의 코드 생략----
+public Customer(int CustomerID, String customerName) //인자를 받는 생성자
+{
+  this.customerID = customerID;
+  this.customerName = customerName;
+  customerGrade = "SILVER";
+  bonusRatio = 0.01;
+}
+----아래 코드 생략----
