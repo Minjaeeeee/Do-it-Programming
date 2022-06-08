13c3ce1fcff41e9a91594ee2bfe5bb9951a9a63e
Min Jae Kim <kmj6832596@naver.com>
2022-06-08 23:25:51 +0900

    Update person.java

--- person/person.java
+++ person/person.java
@@ -1,19 +1,19 @@
 package person;
 
-public class person
+public class Person
 {
     String name;
     float height;
     float weight;
     
-    public person() {} // 디폴트 생성자 추가.
+    public Person() {} // 디폴트 생성자 추가.
 
-    public person(String pname) //매개변수로 받는 것이다.(이름을 받고 person 클래스를 생성)
+    public Person(String pname) //매개변수로 받는 것이다.(이름을 받고 person 클래스를 생성)
     {
         name = pname;
     } //이름을 매개변수로 입력받는 생성자
 
-    public person(String pname, float pheight, float pweight) //이름, 키, 몸무게를 매개변수로 받는 생성자
+    public Person(String pname, float pheight, float pweight) //이름, 키, 몸무게를 매개변수로 받는 생성자
     {
         name = pname;
         height = pheight;
